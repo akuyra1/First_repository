8ef711cec46b2825e283994b57365b189e69dfbf
Ramunas Kazlauskas <akuyra1@gmail.com>
2019-07-15 20:41:49 +0100

    Some small changes

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-name = prompt("What is your name, stranger?")
+//////////////////STALKER EXERCISE/////////////////////
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var age = prompt("How old are you?");
+// console.log("Your name is " + firstName + " "
+//  + lastName + " and you are " + age + " year old!");
 
-console.log(name)
+///////////////age calculator////////////////
+// var year = 365.25
+// var age = prompt("please enter your age")
+// var daysAlive = age * year
+// alert("You are " + daysAlive + " days old.")
+
+/////////////LOGICAL OPERATORS EXERCISE////////////
+//  age = prompt("what is your age?")
+
+//  if (age >= 0 && age < 25) {
+//      alert("You cannot drink alcohol");
+//  } else if (age >= 25 && age <= 35) {
+//      alert("You're getting old!");
+//  } else {
+//      alert("You are too old.")
+//  }
+
+// for(var i = 0; i < 16; i++) {
+//     console.log("i is: " + i);
+// }
+
+//PRINT ALL NUMBERS BETWEEN -10 AND 19
+// for(var i = -11; i < 20; i++){
+//     console.log(i)
+// }
+//PRINT ALL EVEN NUMBERS BETWEEN 10 AND 40
+// for(var i = 10; i <= 40 ; i+=2){
+//     console.log(i);
+// }
+
+//PRINT ALL ODD NUMBERS BETWEEN 300 AND 333
+// for(var i = 301; i <= 333; i+=2){
+//     console.log(i)
+// }
+//IF EVEN PRINT TRUE OR ELSE FALSE FUNCTION///
+// function isEven(i) {
+//     if (i % 2 === 0) {
+//         console.log("True")
+//     } else {
+//         console.log("False")
+//     }
+// }
+// isEven(4)
+
+var variableABC = "A B C"; 
+variableABC = variableABC.replace('B', 'D')
+
